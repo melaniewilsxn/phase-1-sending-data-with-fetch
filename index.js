2222ce2ea1485e9565add9364f1ac3beb4e5d038
Melanie Wilson <melaniemwilson1@gmail.com>
2023-04-18 15:14:03 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    })
+    .then(res => res.json())
+    .then(object => document.querySelector("body").append(object.id))
+    .catch(error => {
+        let errorMessage = "Unauthorized Access"
+        document.querySelector("body").textContent = errorMessage
+    })
+}
